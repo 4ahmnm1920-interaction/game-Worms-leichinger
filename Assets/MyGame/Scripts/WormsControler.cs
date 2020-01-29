aa6ac5003efa290f02e1d6f05f0eddee8bdfb271
leoeichinger <eichinger.l@gmx.at>
2020-01-29 11:29:44 +0100

    addet a spimle player controler

---
+++ Assets/MyGame/Scripts/WormsControler.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WormsControler : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float jumphigth;
+    public float walkspeed;
+    
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Vector3 jump = new Vector3(0, jumphigth, 0);
+            rb.AddForce(jump);
+
+        }
+
+        if(Input.GetKey(KeyCode.D))
+        {
+            Vector3 forwoard = new Vector3(0, 0, walkspeed);
+            rb.AddForce(forwoard);
+
+        }
+
+         if(Input.GetKey(KeyCode.A))
+        {
+            Vector3 backwards = new Vector3(0, 0, -walkspeed);
+            rb.AddForce(backwards);
+
+        }
+    
+    }
+}
